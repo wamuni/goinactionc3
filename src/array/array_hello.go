050abaa8a1032251771df6e2fd1e8f61d1be6288
wamuni <boshi.pan@outlook.com>
2024-05-05 12:42:55 +0100

    some interesting finding for array and slice

--- src/array/array_hello.go
+++ src/array/array_hello.go
@@ -7,15 +7,15 @@ func main() {
 	// First way to declare an array
 	// It will initialize each value as zero-value of the type
 	var arr [5]int
-	print_array(arr[:])
+	print_array_int(arr[:])
 
 	arr_second := [5]int{10, 20, 30, 40, 50}
 	print_separater()
-	print_array(arr_second[:])
+	print_array_int(arr_second[:])
 
 	arr_third := [5]int{1: 20, 2: 40}
 	print_separater()
-	print_array(arr_third[:])
+	print_array_int(arr_third[:])
 
 	// get access of pointer value of array
 	arr_pointer := [5]*int{0: new(int), 1: new(int)}
@@ -27,11 +27,23 @@ func main() {
 		// and if it's nil, then it can't use *
 		fmt.Println(index, num)
 	}
+
+	arr_string := [5]string{"Red", "Green", "Blue", "Yellow", "Orange"}
+	print_array_string(arr_string)
 }
 
-func print_array(arr []int) {
+// TODO: what is the difference between []int and []string
+// Why it allow to pass [5]int as argument to a function with []int as parameter
+// However it doesn't work for [5]string
+func print_array_int(arr []int) {
 	for index, num := range arr {
 		fmt.Printf("The number in position %d is %d\n", index+1, num)
+	}
+}
+
+func print_array_string(arr [5]string) {
+	for index, str := range arr {
+		fmt.Printf("The string in position %d is %s\n", index+1, str)
 	}
 }
 
