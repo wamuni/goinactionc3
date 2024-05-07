78495fa4280061264eab33d6cb7b2a351a418b00
wamuni <boshi.pan@outlook.com>
2024-05-07 02:36:35 +0100

    1. check new_slice and slice 2. check append algorithm

--- src/slice/slice.go
+++ src/slice/slice.go
@@ -30,8 +30,47 @@ func main() {
 	print_slice_len_cap(slice_nil)
 	print_slice_len_cap(slice_empty)
 	print_slice_len_cap(slice_empty_iteral)
+
+	print_separater()
+	slice_to_slice := []int{10, 20, 30, 40, 50} // cap: 5, len: 5
+	print_slice_len_cap_int(slice_to_slice)
+	new_slice := slice_to_slice[1:3]
+	print_slice_len_cap_int(new_slice) // cap: 4, len: 2
+	//! the new slice cap will be the old cap - start index
+	// which means new slice won't have access to the elements before start index
+	//TODO: Does that mean the new slice are pointing at the same array?
+	slice_to_slice[2] = 100
+	fmt.Printf("New change in slice: %d\n", slice_to_slice[2])
+	fmt.Printf("Element in new slice: %d\n", new_slice[1])
+	//? The new slice are pointing as the same array address with the old slice
+
+	print_separater()
+	// append algorithm check
+	// append_algorithm_check()
+
+}
+
+func append_algorithm_check() {
+	//* Through the observation
+	//? 1. it will start to double
+	//? 2. then 1.75 around 500
+	//? 3. around 1.4 around 1000
+	// TODO: Check the append algorithm
+	slice := []int{0, 1, 2, 3, 4, 5}
+	for i := 0; i < 2000; i += 1 {
+		slice = append(slice, i)
+		print_slice_len_cap_int(slice)
+	}
+}
+
+func print_slice_len_cap_int(slice []int) {
+	fmt.Printf("The int slice has length of %d and cap of %d\n", len(slice), cap(slice))
 }
 
 func print_slice_len_cap(slice []string) {
 	fmt.Printf("The slice has length of %d and cap of %d\n", len(slice), cap(slice))
+}
+
+func print_separater() {
+	fmt.Println("=================================================")
 }
