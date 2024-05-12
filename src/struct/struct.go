9a9553e2978b8d7c3e921e66822dde53d5855cb3
wamuni <boshi.pan@outlook.com>
2024-05-12 23:43:50 +0100

    embed struct how to declare it

--- src/struct/struct.go
+++ src/struct/struct.go
@@ -13,6 +13,15 @@ type User struct {
 	privileged bool
 }
 
+// declare a struct within a struct
+type admin struct {
+	person User
+	level  string
+}
+
+// declare a new type for existing type
+type Duration int64
+
 func main() {
 	fmt.Printf("Hello MotherFucker\n")
 	utils.PrintSeprater()
@@ -26,8 +35,30 @@ func main() {
 	}
 	utils.PrintSeprater()
 	printUser(lisa)
+
+	eddie := User{"Eddie", "eddie@email.com", 124, true}
+	utils.PrintSeprater()
+	printUser(eddie)
+
+	// how to declare embeded struct
+	fred := admin{
+		person: User{
+			name:       "Lisa",
+			email:      "lisa@email.com",
+			ext:        123,
+			privileged: true,
+		},
+		level: "super",
+	}
+	utils.PrintSeprater()
+	printAdmin(fred)
 }
 
 func printUser(u User) {
 	fmt.Printf("Name:\t\t%s,\nEmail:\t\t%s,\nExt:\t\t%d,\nPrivileged:\t%t,\n", u.name, u.email, u.ext, u.privileged)
+}
+
+func printAdmin(ad admin) {
+	printUser(ad.person)
+	fmt.Printf("Level:\t\t%s\n", ad.level)
 }
