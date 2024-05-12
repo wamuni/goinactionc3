269aec325259a373730157c360c14f106e768f7f
wamuni <boshi.pan@outlook.com>
2024-05-12 22:55:09 +0100

    how to declare a struct

--- src/struct/struct.go
+++ src/struct/struct.go
@@ -6,7 +6,28 @@ import (
 	"chapter3.wamuni/m/src/utils"
 )
 
+type User struct {
+	name       string
+	email      string
+	ext        int
+	privileged bool
+}
+
 func main() {
 	fmt.Printf("Hello MotherFucker\n")
 	utils.PrintSeprater()
+	var bill User // initialize with nothing, then it will only get zero-value
+	printUser(bill)
+	lisa := User{
+		name:       "Lisa",
+		email:      "lisa@email.com",
+		ext:        123,
+		privileged: true,
+	}
+	utils.PrintSeprater()
+	printUser(lisa)
+}
+
+func printUser(u User) {
+	fmt.Printf("Name:\t\t%s,\nEmail:\t\t%s,\nExt:\t\t%d,\nPrivileged:\t%t,\n", u.name, u.email, u.ext, u.privileged)
 }
