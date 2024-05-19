04592f48f97b69b53ec315144cbeff8d6bd1c471
wamuni <boshi.pan@outlook.com>
2024-05-20 00:37:47 +0100

    change the email to Email

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -5,3 +5,19 @@ import "fmt"
 func PrintSeprater() {
 	fmt.Printf("-----------------------------\n")
 }
+
+type alterCounter int
+
+func New(value int) alterCounter {
+	return alterCounter(value)
+}
+
+type user struct {
+	Name  string
+	Email string
+}
+
+type Admin struct {
+	user
+	Right int
+}
